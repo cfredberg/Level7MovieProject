42db7a22f8f579ff437754c3cbfaf9e66b23fdca
cfredberg <charliefredberg@outlook.com>
2023-05-28 12:56:30 -0700

    having issues with part 3

--- src/main/java/com/movies/Movies/MoviesProjectApplication.java
+++ src/main/java/com/movies/Movies/MoviesProjectApplication.java
@@ -1,4 +1,4 @@
-package com.movies.Movies.Project;
+package com.movies.Movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
