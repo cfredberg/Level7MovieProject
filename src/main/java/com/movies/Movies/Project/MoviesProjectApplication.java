3af4c289ae1c995ae039e4e4b5f719c5267ffe84
cfredberg <charliefredberg@outlook.com>
2023-05-27 17:18:55 -0700

    initial

---
+++ src/main/java/com/movies/Movies/Project/MoviesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.movies.Movies.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesProjectApplication.class, args);
+	}
+
+}
