366fa5da010e5bd17efb9719d9fc9811bbc90a55
cfredberg <charliefredberg@outlook.com>
2023-06-17 17:49:50 -0700

    progress made on 6/17

--- src/main/java/com/movies/Movies/repository/LocRepository.java
+++ src/main/java/com/movies/Movies/repository/LocRepository.java
@@ -1,12 +1,33 @@
 package com.movies.Movies.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Repository
 public class LocRepository {
+    private final WebClient webClient;
+    private static final String baseUrl = "https://api.themoviedb.org/3/search/movie";
+
+    public LocRepository() {
+        webClient = WebClient
+                .builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
 
     public String getResults(String query){
-        return "Searching for books related to " + query;
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("language", "en-US")
+                        .queryParam("api_key", "ccb1d2d267cfb02bf8df578ecca952cb")
+                        .queryParam("query", query)
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
     }
 
 }
