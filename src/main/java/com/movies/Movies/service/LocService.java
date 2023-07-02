f23b78ebecb3a47e029cee71aa0f125fe48cfc22
cfredberg <charliefredberg@outlook.com>
2023-07-01 18:00:07 -0700

    progress made on 7/1

--- src/main/java/com/movies/Movies/service/LocService.java
+++ src/main/java/com/movies/Movies/service/LocService.java
@@ -1,12 +1,17 @@
 package com.movies.Movies.service;
 
+import com.movies.Movies.repository.LocRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.spi.LocaleNameProvider;
 
 @Service
 public class LocService {
 
     public String getResults(String query){
-        return "Searching for books related to " + query;
+        LocRepository locrepo = new LocRepository();
+        return locrepo.getResults(query);
     }
 
 }
