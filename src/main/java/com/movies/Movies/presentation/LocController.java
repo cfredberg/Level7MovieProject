df2493b3623c6382bb26c4d516255d42441ec715
cfredberg <charliefredberg@outlook.com>
2023-07-22 18:05:09 -0700

    Pretty much finished level 7git add --allgit add --allgit add --all!

--- src/main/java/com/movies/Movies/presentation/LocController.java
+++ src/main/java/com/movies/Movies/presentation/LocController.java
@@ -12,6 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class LocController {
 
     private final LocService locService;
